ce16523d734e9ce4eefe4678f3613667c76bfe0a
qgao <qgao@student-bmhs-la.org>
2023-01-26 13:51:46 -0800

    Day 4

--- src/Game.java
+++ src/Game.java
@@ -1,33 +1,149 @@
 public class Game {
-    private Room currentRoom;
 
-    public Game(){
+    private Room currentRoom;
+    private Parser parser;
+    private Player player;
+
+
+    public Game() {
+        parser = new Parser();
+        player = new Player();
     }
 
-    public void creatRooms(){
-        Room centerGarden= new Room("Short describe");
-        Room northGarden= new Room("Short describe");
-        Room southGarden= new Room("Short describe");
-        Room eastGarden= new Room("Short describe");
-        Room westGarden= new Room("Short describe");
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.createRooms();
+        game.play();
+    }
+
+    private void createRooms() {
+        Room centerGarden = new Room("centerGarden short description goes here", "center long descrip");
+        Room northGarden = new Room("northGarden short description goes here", "north long descrip");
+        Room southGarden = new Room("southGarden short description goes here", "south long descrip");
+        Room eastGarden = new Room("eastGarden short description goes here", "east long descrip");
+        Room westGarden = new Room("westGarden short description goes here", "west long descrip");
+
+        centerGarden.setExit("north", northGarden);
+        centerGarden.setExit("south", southGarden);
+        // ... continued
+
+        eastGarden.setExit("west", centerGarden);
+
+        Item obj = new Item();
+        Item obj2 = new Item();
+
+        player.setItem("one", obj);
+        centerGarden.setItem("two", obj2);
+
+        currentRoom = centerGarden;
     }
 
     public void play() {
         printWelcome();
+
         boolean finished = false;
-        while(!finished){
+
+        while(!finished) {
+            Command command = parser.getCommand();
+            finished = processCommand(command);
         }
-        System.out.println("Thanks for playing");
+        System.out.println("Thanks for playing!");
     }
 
-    private void printWelcome(){
-        System.out.println();
-        System.out.println("welcome to my game");
-        System.out.println("You will find your self in a garden");
-        System.out.println("Type help if you need help");
-        System.out.println();
-        System.out.println();
+    private boolean processCommand(Command command) {
+        boolean wantToQuit = false;
+
+        CommandWord commandWord = command.getCommandWord();
+
+        switch(commandWord) {
+            case UNKNOWN:
+                System.out.println("I don't know what you mean");
+                break;
+
+            case HELP:
+                printHelp();
+                break;
+
+            case GO:
+                goRoom(command);
+                break;
+
+            case QUIT:
+                wantToQuit = quit(command);
+                break;
+
+            case LOOK:
+                look(command);
+                break;
+
+            case DROP:
+                break;
+
+            case GRAB:
+                break;
+        }
+
+        return wantToQuit;
     }
+
+    private void printHelp() {
+        System.out.println("You are lost.  You are alone.  You wander");
+        System.out.println("You are in a garden maze");
+        System.out.println();
+        System.out.println("Your command words are:");
+        parser.showCommands();
+    }
+
+    private void look(Command command) {
+        if(command.hasSecondWord()) {
+            System.out.println("You can't look at " + command.getSecondWord());
+            return;
+        }
+
+        System.out.println(currentRoom.getLongDescription());
+        System.out.println(player.getItemString());
+    }
+
+    private void goRoom(Command command)
+    {
+        if(!command.hasSecondWord()) {
+            // if there is no second word, we don't know where to go...
+            System.out.println("Go where?");
+            return;
+        }
+
+        String direction = command.getSecondWord();
+
+        // Try to leave current room.
+        Room nextRoom = currentRoom.getExit(direction);
+
+        if (nextRoom == null) {
+            System.out.println("There is no door!");
+        }
+        else {
+            currentRoom = nextRoom;
+            System.out.println(currentRoom.getShortDescription());
+        }
+    }
+
+    private boolean quit(Command command) {
+        if(command.hasSecondWord()) {
+            System.out.println("You can't quit " + command.getSecondWord());
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    private void printWelcome() {
+        System.out.println();
+        System.out.println("Welcome to my text adventure game!");
+        System.out.println("You will find yourself in a garden maze, desperate to escape!");
+        System.out.println("Type \"help\" if you need assistance");
+        System.out.println();
+        System.out.println("we will print a long room description here");
+    }
+
+
 }
-
-
--- src/Room.java
+++ src/Room.java
@@ -1,22 +1,62 @@
 import java.util.HashMap;
+
+import java.util.Set;
+import java.util.Iterator;
+
 public class Room {
     private String description;
-    private HashMap<String, Room>exits;
+    private HashMap<String, Room> exits;
+    private HashMap<String, Item> inventory;
 
-    public Room(String description) {
+    private String longDescription;
+
+    public Room(String description, String longDescription) {
         this.description = description;
-        exits = new HashMap< String,Room>();
+        this.longDescription = longDescription;
+        exits = new HashMap<String, Room>();
+        inventory = new HashMap<>();
     }
 
-    public void setExit(String direction, Room neighbor){
-        exits.put (direction, neighbor);
+    private String getExitString() {
+        String returnString = "Exits:";
+        Set<String> keys = exits.keySet();
+        for(String exit: keys) {
+            returnString += " " + exit;
+        }
+        return returnString;
     }
 
-    public Room getExit(String direction){
-        return exits.get (direction);
+    public String getLongDescription() {
+        return longDescription + "\n" + getExitString() + "\n" + getItemString();
     }
 
-    public String  getShortDescription() {
+    public void setExit(String direction, Room neighbor) {
+        exits.put(direction, neighbor);
+    }
+
+    public Room getExit(String direction) {
+        return exits.get(direction);
+    }
+
+    public void setItem(String name, Item item) {
+        inventory.put(name, item);
+    }
+
+    public Item getItem(String name) {
+        return inventory.remove(name);
+    }
+
+    public String getItemString() {
+        String returnString = "Room Inv:";
+        Set<String> keys = inventory.keySet();
+        for(String item: keys) {
+            returnString += " " + item;
+        }
+        return returnString;
+    }
+
+    public String getShortDescription() {
         return description;
     }
+
 }
